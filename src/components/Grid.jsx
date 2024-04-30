1ddc5ac5acae0594ef9803fd4a44da2075ae248c
Terminator1601 <rishisharma4201@gmail.com>
2024-04-30 16:03:15 +0530

    board created

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import './Grid.css'; // Import CSS file for styling
 
 const Grid = () => {
+
+  const greengrid= Array.from({length:4},(_, rowIndex)=>(
+    <div key={rowIndex} className='row'>
+      {Array.from({length :7},(_,colIndex)=>(
+        <div key={colIndex} className='cell blue'></div>
+      ))}
+      {Array.from({length :6},(_,colIndex)=>(
+        <div key={colIndex} className='cell green'></div>
+      ))}
+      {Array.from({length :7},(_,colIndex)=>(
+        <div key={colIndex} className='cell blue'></div>
+      ))}
+
+    </div>
+  ));
+
+
   // Create an array to represent rows
   const rows = Array.from({ length: 3 }, (_, rowIndex) => (
     // Create an array to represent columns
@@ -16,6 +33,7 @@ const Grid = () => {
   return (
     <div className="grid-container">
       {rows}
+      {greengrid}
       {rows}
     </div>
   );
