142e07f9bd819d31813a3b0a104efe7da6f7b97f
Terminator1601 <rishisharma4201@gmail.com>
2024-04-30 17:15:06 +0530

    made some changes in the logic

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,42 +1,81 @@
-import React from 'react';
-import './Grid.css'; // Import CSS file for styling
+// import React from "react";
+// import "./Grid.css"; // Import CSS file for styling
+
+// const Grid = () => {
+//   const grid = [];
+//   const gridRows = 10;
+//   const gridCols = 20;
+//   const actualRows = Math.floor(gridRows / 3);
+//   const actualCols = Math.floor(gridCols / 3);
+//   console.log(actualRows);
+//   console.log(actualCols);
+//   for (let index = 0; index < gridRows; index++) {
+//     if (index < actualRows) {
+//       grid.push(
+//         <div key={`blueRow${index}`} className="row">
+//           {Array.from({ length: 20 }, (_, colIndex) => (
+//             <div key={`blueCell${colIndex}`} className="cell blue"></div>
+//           ))}
+//         </div>
+//       );
+//     }
+//     else if (index>=actualRows){
+//       for (let indexCol = 0; indexCol < actualCols; indexCol++) {
+//         if (indexCol < actualCols) {
+//           grid.push(
+//             <div key={`blueRow${indexCol}`} className="row">
+//               {Array.from({ length: 20 }, (_, colIndex) => (
+//                 <div key={`blueCell${colIndex}`} className="cell green"></div>
+//               ))}
+//             </div>
+//           );
+
+//       }
+//     }
+//   }
+
+//   return (
+//     <div className="grid-container">
+
+//       {grid}
+//     </div>
+//   );
+// };
+
+// export default Grid;
+
+import React from "react";
+import "./Grid.css"; // Import CSS file for styling
 
 const Grid = () => {
+  const grid = [];
+  const gridRows = 10;
+  const gridCols = 20;
+  const actualRows = Math.floor(gridRows / 3);
+  const actualCols = Math.floor(gridCols / 3);
 
-  const greengrid= Array.from({length:4},(_, rowIndex)=>(
-    <div key={rowIndex} className='row'>
-      {Array.from({length :7},(_,colIndex)=>(
-        <div key={colIndex} className='cell blue'></div>
-      ))}
-      {Array.from({length :6},(_,colIndex)=>(
-        <div key={colIndex} className='cell green'></div>
-      ))}
-      {Array.from({length :7},(_,colIndex)=>(
-        <div key={colIndex} className='cell blue'></div>
-      ))}
+  for (let rowIndex = 0; rowIndex < gridRows; rowIndex++) {
+    grid.push(
+      <div key={`row${rowIndex}`} className="row">
+        {Array.from({ length: gridCols }, (_, colIndex) => {
+          const isGreenCell =
+            rowIndex >= actualRows &&
+            rowIndex < actualRows * 2 &&
+            colIndex >= actualCols &&
+            colIndex < actualCols * 2;
 
-    </div>
-  ));
+          return (
+            <div
+              key={`cell${colIndex}`}
+              className={`cell ${isGreenCell ? "green" : "blue"}`}
+            ></div>
+          );
+        })}
+      </div>
+    );
+  }
 
-
-  // Create an array to represent rows
-  const rows = Array.from({ length: 3 }, (_, rowIndex) => (
-    // Create an array to represent columns
-    <div key={rowIndex} className="row">
-      {Array.from({ length: 20 }, (_, colIndex) => (
-        // Create a cell with blue background color
-        <div key={colIndex} className="cell blue"></div>
-      ))}
-    </div>
-  ));
-
-  return (
-    <div className="grid-container">
-      {rows}
-      {greengrid}
-      {rows}
-    </div>
-  );
+  return <div className="grid-container">{grid}</div>;
 };
 
 export default Grid;
